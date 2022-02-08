c0f2cbde3ce6748cd6941ac0ab32379ac77196f2
Leandro Cortiz <49104364+leandrozitroc@users.noreply.github.com>
2022-02-08 07:14:38 -0300

    Update fizzBuzz.ts

--- fizzBuzz.ts
+++ fizzBuzz.ts
@@ -1,7 +1,7 @@
 function fizzbuzz(numero: number): string | number {
-  if (num % 15 === 0) return 'FizzBuzz';
-  if (num % 5 === 0) return 'Buzz';
-  if (num % 3 === 0) return 'Fizz';
+  if (numero % 15 === 0) return 'FizzBuzz';
+  if (numero % 5 === 0) return 'Buzz';
+  if (numero % 3 === 0) return 'Fizz';
   return numero;
 }
 
